181880f66d24cbe98b1125be3764de73e11bce90
Adrián Plávka <adrian.plavka@gmail.com>
2026-03-06 22:16:50 +0100

    Bump Ecto versions

--- mix.exs
+++ mix.exs
@@ -34,9 +34,9 @@ defmodule Terminator.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ecto, "~> 3.0"},
-      {:ecto_sql, "~> 3.0"},
-      {:postgrex, "~> 0.15.0"},
+      {:ecto, ">= 3.11.0"},
+      {:ecto_sql, ">= 3.11.0"},
+      {:postgrex, ">= 0.18.0"},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:optimus, "~> 0.1.0", only: :dev},
       {:mix_test_watch, "~> 0.8", only: :dev, runtime: false},
